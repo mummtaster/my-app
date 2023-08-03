a948c112d80d548e451493365eb28f809b57b9cf
mummtaster <mummtaster@gmail.com>
2023-08-04 00:05:21 +0900

    To Do List part one ,Coin Tracker 학습

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Button from "./Button";
 import styled from "./App.module.css";
 import {useState, useEffect} from "react";
 
-function App() {
+/*function App() {
 
     const [counter, setValue] = useState(0);
     const [keyword, setKeyword] = useState("");
@@ -30,6 +30,69 @@ function App() {
             <h1>{counter}</h1>
             <button onClick={onClick}>Click Me</button>
         </div>
+    );
+}*/
+
+// To Do List part one
+/*function App(){
+    const [toDo , setTodo] = useState("");
+    const [toDos , setTodos] = useState([]);
+    const onChange = (event) => setTodo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if(toDo === ""){
+            return;
+        }
+        setTodo("");
+        setTodos((currentArray) => [toDo , ...currentArray]);
+
+    }
+    console.log(toDos);
+    return(
+        <div>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="text"
+                    placeholder="Write your to do..."
+                />
+                <button>Add To Do</button>
+            </form>
+            <hr />
+            <ul>
+                {toDos.map((item , index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+
+        </div>
+    );
+}*/
+// Coin Tracker
+function App(){
+    const [loading , setLoading] = useState(true);
+    const [coins , setCoins] = useState([]);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+    }, []);
+    return(
+      <div>
+          <h1>The Coins! ({loading ? "" : coins.length})</h1>
+          {loading ? <strong>Loading....</strong> : (
+              <select>
+                  {coins.map((coin , index) => (
+                      <option key={index}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</option>
+                  ))}
+              </select>
+          )}
+      </div>
     );
 }
 
